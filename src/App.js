3f040db1833b4b3f8f6bb89705ca12758125e0b4
Michael Porter <michael.porter@utsouthwestern.edu>
2019-06-11 10:13:52 -0500

    all tests passing, now need to style

--- src/App.js
+++ src/App.js
@@ -1,36 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import DrumPad from './DrumPad';
 
-function App() {
-  return (
-    <div>
-      <div id="display">
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      displayMsg: "Let's jam!"
+    };
+
+    this.updateDisplayMsg = this.updateDisplayMsg.bind(this);
+  }
+
+  updateDisplayMsg(msg) {
+    this.setState({
+      displayMsg: msg
+    });
+  }
+
+  render() {
+
+    return (
+      <div>
+        <div id="display">
+          {this.state.displayMsg}
+        </div>
+
+        <div id="pad">
+          <DrumPad title="Heater-1" keyMap="Q" keyCode="81" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" updateMsgCallback={this.updateDisplayMsg}/>
+          
+          <DrumPad title="Heater-2" keyMap="W" keyCode="87" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" updateMsgCallback={this.updateDisplayMsg}/>
+          
+          <DrumPad title="Heater-3" keyMap="E" keyCode="69" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" updateMsgCallback={this.updateDisplayMsg}/>
+          
+          <DrumPad title="Heater-4" keyMap="A" keyCode="65" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" updateMsgCallback={this.updateDisplayMsg}/>
+          
+          <DrumPad title="Clap" keyMap="S" keyCode="83" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" updateMsgCallback={this.updateDisplayMsg}/>
+          
+          <DrumPad title="Open-HH" keyMap="D" keyCode="68" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" updateMsgCallback={this.updateDisplayMsg}/>
+          
+          <DrumPad title="Kick-n'-Hat" keyMap="Z" keyCode="90" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" updateMsgCallback={this.updateDisplayMsg}/>
+          
+          <DrumPad title="Kick" keyMap="X" keyCode="88" audioURL="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" updateMsgCallback={this.updateDisplayMsg}/>
+          
+          <DrumPad title="Closed-HH" keyMap="C" keyCode="67" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" updateMsgCallback={this.updateDisplayMsg}/>
+        </div>
+
       </div>
 
-      <div id="pad">
-        <DrumPad title="bass1" keyMap="Q" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"/>
-        
-        <DrumPad title="bass2" keyMap="W" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"/>
-        
-        <DrumPad title="bass3" keyMap="E" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"/>
-        
-        <DrumPad title="bass4" keyMap="A" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"/>
-        
-        <DrumPad title="bass5" keyMap="S" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"/>
-        
-        <DrumPad title="bass6" keyMap="D" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"/>
-        
-        <DrumPad title="bass7" keyMap="Z" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"/>
-        
-        <DrumPad title="bass8" keyMap="X" audioURL="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"/>
-        
-        <DrumPad title="bass9" keyMap="C" audioURL="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"/>
-      </div>
-
-    </div>
-
-  );
+    );
+  }
 }
 
 export default App;
--- src/DrumPad.js
+++ src/DrumPad.js
@@ -7,15 +7,37 @@ class DrumPad extends Component {
     this.state = {
 
     };
+
+    this.playAudio = this.playAudio.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress(e) {
+    if(e.keyCode === parseInt(this.props.keyCode)) {
+      this.playAudio();
+    }
+  }
+
+  playAudio(e) {
+    const audioElem = document.getElementById(this.props.keyMap);
+    audioElem.currentTime = 0;
+    audioElem.play();
+    this.props.updateMsgCallback(this.props.title);
   }
 
   render() {
     return (
-      <div className="drum-pad" id={this.props.title}>
+      <div className="drum-pad" id={this.props.title} onClick={this.playAudio}>
         {this.props.keyMap}
-        <audio className="clip" id={this.props.keyMap}>
-          <source src={this.props.audioURL} />
-        </audio>
+        <audio className="clip" id={this.props.keyMap} src={this.props.audioURL} />
       </div>
     )
   }
